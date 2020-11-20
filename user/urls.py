27e393d071f877667da491dc9d6ee1976dc4f795
mostlypanda <sarthakmittal1461@gmail.com>
2020-11-20 15:11:50 +0530

    some changes

--- user/urls.py
+++ user/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 from .views import *
 urlpatterns = [
+    path('signup/',signup.as_view(), name ='signup'),
     path('sendotp/<str:email1>/',OTP),
     path('sendotp_fp/<str:email1>/',OTP_fp),
-    path('signup/',signup.as_view(), name ='signup'),
     path('FP/',FP.as_view(), name ='FP'),
-    path('addmentor/', addmentor.as_view(), name ='addmentor'),
     path('profile/', profile.as_view(), name ='profile'),
+    path('addmentor/', addmentor.as_view(), name ='addmentor'),
     path('buysell/', addbuysell.as_view(), name ='addbuysell'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
